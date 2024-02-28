85abf581e2e169d3f77aad91e11fd5eb2c53a1ec
karishmaaaaa <karishma.ss@kalvium.community>
2024-02-28 10:54:07 +0530

    added cors

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require('express')
 const bodyParser=require('body-parser')
 const app = express()
 const port = 3000
+const cors = require('cors')
 const {getRouter, postRouter,deleteRouter,putRouter} = require("./Routes/routes")
 app.use(bodyParser.json())
+app.use(cors())
 app.get('/', (req, res) => {
     res.json({
         message: 'o_O',
